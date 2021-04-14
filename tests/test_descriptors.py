e69f9362dd85d9aac99b2b56794d7b7c26b4dc2b
Joris Paret <joris.paret@umontpellier.fr>
2021-04-14 14:50:29 +0200

    Add basic tests

--- tests/test_descriptors.py
+++ tests/test_descriptors.py
@@ -7,6 +7,7 @@ from pysc.trajectory import Trajectory
 from pysc.descriptor import RadialDescriptor, AngularDescriptor
 from pysc.descriptor import BondOrientationalDescriptor, LechnerDellagoDescriptor
 
+from numpy import float32
 
 class Test(unittest.TestCase):
 
@@ -17,27 +18,40 @@ class Test(unittest.TestCase):
 
     def _compute(self, D):
         D.add_filter("species == 'B'", group=0)
-        X = D.compute()
+        D.compute()
         
     def test_radial(self):
-        D = RadialDescriptor(self.traj, rlim=(0.0, 2.5))
+        D = RadialDescriptor(self.traj, dr=0.1)
         self._compute(D)
-        
+        # check automatically computed bounds
+        self.assertEqual(D.rlim, (0.05, 2.45), 'wrong bounds for the radial grid')
+        # check average value of g(r) at the first peak
+        gr = D.normalize_gr(D.average)
+        self.assertEqual(float32(gr[8]), float32(2.939288),
+                         'wrong average value at the first peak of g(r)')
+
     def test_angular(self):
-        D = AngularDescriptor(self.traj)
+        D = AngularDescriptor(self.traj, dtheta=3.0)
         D.cutoffs = self.cutoffs
         self._compute(D)
+        q = D.normalize_sin(D.average)
+        self.assertEqual(float32(q[22]), float32(0.015544709),
+                         'wrong average value at the peak \theta=67.5°')
         
     def test_steinhardt(self):
         D = BondOrientationalDescriptor(self.traj)
         D.cutoffs = self.cutoffs
         self._compute(D)
+        # test value of q_1
+        self.assertEqual(float32(D.average[1]), float32(0.09393699),
+                         'wrong average value for q_1')
         
     def test_lechner_dellago(self):
         D = LechnerDellagoDescriptor(self.traj)
         D.cutoffs = self.cutoffs
         self._compute(D)
-
+        self.assertEqual(float32(D.average[1]), float32(0.02164681),
+                         'wrong average value for qbar_1')
 
 if __name__ == '__main__':
     unittest.main()
