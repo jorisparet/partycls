e0588e427ed9ac7fe5e4549c416c9b54194ff68f
Joris Paret <joris.paret@umontpellier.fr>
2021-05-20 15:19:46 +0200

    Allow change in grid and more flexibility for BOP after instanciation

--- pysc/descriptor/bo.py
+++ pysc/descriptor/bo.py
@@ -7,11 +7,19 @@ class BondOrientationalDescriptor(AngularStructuralDescriptor):
     name = 'bond-orientational'
     symbol = 'bo'
     
-    def __init__(self, trajectory, lmax=10):
+    def __init__(self, trajectory, lmin=0, lmax=8, orders=None):
         AngularStructuralDescriptor.__init__(self, trajectory)
         if self.trajectory[0].number_of_dimensions == 2:
             raise ValueError('trajectory must be 3-dimensional to be used with a {} descriptor'.format(self.name))
-        self.grid = numpy.array(range(0, lmax))
+        self._bounds(lmin, lmax, orders)
+
+    @property    
+    def orders(self):
+        return self.grid
+    
+    @orders.setter
+    def orders(self, values):
+        self._bounds(0, 8, values)
 
     @property
     def n_features(self):
@@ -34,52 +42,30 @@ class BondOrientationalDescriptor(AngularStructuralDescriptor):
                 # compute BO parameters for particle `i`
                 neigh_i = self.neighbors[n][i]
                 hist_n_i = numpy.empty_like(self.grid, dtype=numpy.float64)
-                for l in self.grid:
-#                    hist_n_i[l] = self._q_l(l, i, neigh_i, pos_0[n][i], pos_1[n], box)
-                    hist_n_i[l] = compute.ql(l, neigh_i, pos_0[n][i], pos_1[n].T, box)
+                for ln, l in enumerate(self.grid):
+                    hist_n_i[ln] = compute.ql(l, neigh_i, pos_0[n][i], pos_1[n].T, box)
                 features[row] = hist_n_i
                 row += 1      
         self.features = features
         return features
-
-#    def _rotational_invariant(self, l, q_lm):
-#        s = numpy.sum(q_lm*q_lm.conj(), axis=0)
-#        s = numpy.real(s)
-#        q_l = numpy.sqrt( 4.0*numpy.pi / (2*l+1) * s )
-#        return q_l
-    
-#    def _q_l(self, l, i, neigh_i, pos_i, pos_j, box):
-#        """
-#        Rotational invariant of order l for particle `i`.
-#        """
-#        q_lm = self._q_lm(l, i, neigh_i, pos_i, pos_j, box)
-#        return self._rotational_invariant(l, q_lm)
-    
-#    def _q_lm(self, l, i, neigh_i, pos_i, pos_j, box):
-#        q_lm = numpy.zeros(2*l+1, dtype=complex)
-#        # r_ij (cartesian)
-#        r_xyz = pos_j[neigh_i] - pos_i
-#        r_xyz = pbc(r_xyz, box)
-#        # r_ij (spherical)
-#        r_sph = cartesian_to_spherical(r_xyz)
-#        for m in range(2*l+1):
-#            Y_lm = sph_harm(m-l, l, r_sph[:,1], r_sph[:,2])
-#            q_lm[m] = numpy.average(Y_lm)
-#        return q_lm
-    
-#    def normalize(self, dist):
-#        return dist * (1.0 / numpy.sum(dist))
     
     def normalize(self, dist):
         return dist
+
+    def _bounds(self, lmin, lmax, orders):
+        if orders is None:
+            self.grid = numpy.array(range(lmin, lmax+1))
+        else:
+            self.grid = numpy.array(orders)
     
 class LechnerDellagoDescriptor(BondOrientationalDescriptor):
 
     name = 'lechner-dellago'
     symbol = 'ld'
     
-    def __init__(self, trajectory, lmax=10):
-        BondOrientationalDescriptor.__init__(self, trajectory, lmax=lmax)
+    def __init__(self, trajectory, lmin=0, lmax=8, orders=None):
+        BondOrientationalDescriptor.__init__(self, trajectory, lmin=lmin, 
+                                             lmax=lmax, orders=orders)
 
     def compute(self):
         StructuralDescriptor.sanity_checks(self)
@@ -111,8 +97,8 @@ class LechnerDellagoDescriptor(BondOrientationalDescriptor):
                     neigh_neigh_i.append(neigh_j)
                 # compute BO parameters for particle `i`
                 hist_n_i = numpy.empty_like(self.grid, dtype=numpy.float64)
-                for l in self.grid:
-                    hist_n_i[l] = self._qbar_l(l, i, neigh_i, neigh_neigh_i, pos_0[n][i], pos_1[n], box)
+                for ln, l in enumerate(self.grid):
+                    hist_n_i[ln] = self._qbar_l(l, i, neigh_i, neigh_neigh_i, pos_0[n][i], pos_1[n], box)
                     #TODO: fix the shape of neigh_neigh_i to pass it to qbarl()
                     #hist_n_i[l] = compute.qbarl(l, numpy.array(neigh_i), numpy.array(neigh_neigh_i).T, pos_0[n][i], pos_1[n].T, box)
                 features[row] = hist_n_i
@@ -122,12 +108,10 @@ class LechnerDellagoDescriptor(BondOrientationalDescriptor):
     
     def _qbar_lm(self, l, i, neigh_i, neigh_neigh_i, pos_i, pos_j, box):
         Nbar_b = len(neigh_i) + 1
-#        q_lm_i = self._q_lm(l, i, neigh_i, pos_i, pos_j, box)
         q_lm_i = compute.qlm(l, neigh_i, pos_i, pos_j.T, box)
         q_lm_k = []
         for kn in range(len(neigh_i)):
             k = neigh_i[kn]
-#            q_lm_k.append(self._q_lm(l, k, neigh_neigh_i[kn], pos_j[k], pos_j, box))            
             q_lm_k.append(compute.qlm(l, neigh_neigh_i[kn], pos_j[k], pos_j.T, box))            
         qbar_lm = q_lm_i + numpy.sum(q_lm_k, axis=0)
         return qbar_lm / Nbar_b
@@ -138,4 +122,3 @@ class LechnerDellagoDescriptor(BondOrientationalDescriptor):
         """
         qbar_lm = self._qbar_lm(l, i, neigh_i, neigh_neigh_i, pos_i, pos_j, box)
         return compute.rotational_invariant(l, qbar_lm)
-#        return self._rotational_invariant(l, qbar_lm)
