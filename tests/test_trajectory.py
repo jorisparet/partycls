39cf9c1bfccaa49e75d4be4b8009eb3231e0ebfa
Joris Paret <joris.paret@umontpellier.fr>
2021-06-08 19:20:16 +0200

    Add test for get_property and set_property

--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -19,6 +19,43 @@ class Test(unittest.TestCase):
         traj = Trajectory(os.path.join(data, 'dislocation.xyz'), fmt='xyz')
         self.assertEqual(len(traj[0].particle), 27)
 
+    def test_get_property(self):
+        data = os.path.join(os.path.dirname(__file__), '../data/')
+        traj = Trajectory(os.path.join(data, 'traj_with_masses.xyz'), 
+                          additional_fields=['mass'])
+        
+        # full dump
+        dumps = {'pos[0]': [0.1, 0.0, 0.0],
+                 'x': [0.1, 0.0, 0.0],
+                 'mass': [0.5, 0.25, 0.25],
+                 'particle.mass': [0.5, 0.25, 0.25],
+                 'cell.side': [1.0, 1.0, 1.0]}
+        for key, value in  dumps.items():
+            self.assertEqual(value, list(traj[0].dump(key)))
+            
+        # subset
+        dumps = {'pos[0]': [0.1],
+                 'x': [0.1],
+                 'mass': [0.5],
+                 'particle.mass': [0.5]}
+        for key, value in  dumps.items():
+            self.assertEqual(value, list(traj[0].dump(key, "species == 'A'")))        
+    
+    def test_set_property(self):
+        data = os.path.join(os.path.dirname(__file__), '../data/')
+        traj = Trajectory(os.path.join(data, 'traj_with_masses.xyz'), 
+                          additional_fields=['mass'])
+        
+        # cell
+        traj.set_property('cell.side[0]', 2.0)
+        self.assertEqual(traj[0].cell.side[0], 2.0)
+        
+        # particle (full)
+        traj.set_property('radius', 0.4)
+        self.assertEqual(list(traj[0].dump('radius')), [0.4, 0.4, 0.4])
+        traj.set_property('radius', 0.7, "species == 'A'")
+        self.assertEqual(list(traj[0].dump('radius', "species == 'A'")), [0.7])
+
     def test_angular_zscore_pca_kmeans(self):
         D = BondAngleDescriptor(self.traj)
         D.cutoffs = self.cutoffs
