9ddbb975b5c59db0f33c9108ea40a92e8864940a
Joris Paret <joris.paret@umontpellier.fr>
2021-06-15 19:46:56 +0200

    Use setuptools

--- setup.py
+++ setup.py
@@ -4,47 +4,45 @@ import os
 import glob
 
 # We use numpy distutils to compile and wrap f90 code via f2py
-from numpy.distutils.core import setup, Extension
-
-# Get the long description from README.md and try to convert it to
-# reST. Adapted from https://bons.ai/blog/markdown-for-pypi
-# This is necessary to have nicely formatted README on pypi until we
-# understand how to make setuptools work nicely with f2py
-try:
-    from pypandoc import convert
-    readme = convert('README.md', 'rst')
-except (ImportError, OSError):
-    try:
-        readme = open('README.md', 'r').read()
-    except:
-        readme = ''
+import setuptools
+    
+with open('README.md', 'r') as fh:
+    readme = fh.read()
 
 with open('partycls/core/_version.py') as f:
     exec(f.read())
 
 args = dict(name='partycls',
             version=__version__,
-            description='Structural clustering through correlation functions',
+            description='Unsupervised learning of the structure of particulate systems',
             long_description=readme,
+            long_description_content_type="text/markdown",
             author='Joris Paret',
             author_email='joris.paret@umontpellier.fr',
             packages=['partycls',
                       'partycls/core',
                       'partycls/descriptor'],
             install_requires=['numpy', 'sklearn'],
-            ext_modules=[Extension('partycls.descriptor.realspace_wrap',
-                                   sources=['partycls/descriptor/realspace.f90'],
-                                   extra_f90_compile_args=[])],
             license='GPLv3',
             setup_requires = ['numpy'],
             classifiers=[
                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-                'Development Status :: 4 - Beta',
+                'Development Status :: 5 - Production/Stable',
                 'Intended Audience :: Science/Research',
                 'Programming Language :: Python :: 3',
                 'Programming Language :: Python :: 3.7',
-                'Topic :: Scientific/Engineering :: Physics',
-            ]
+                'Topic :: Scientific/Engineering :: Physics'
+                ]
 )
+
+try:
+    from numpy.distutils.core import setup, Extension
+    
+    args["ext_modules"] = [Extension('partycls.descriptor.realspace_wrap',
+                                     sources=['partycls/descriptor/realspace.f90'],
+                                     extra_f90_compile_args=[])]
+
+except (ModuleNotFoundError, ImportError):
+    from distutils.core import setup
 
 setup(**args)
