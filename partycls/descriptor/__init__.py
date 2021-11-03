87d8e863a4c172b1c2021593e158e8480eb92a3f
Joris Paret <joris.paret@umontpellier.fr>
2021-11-03 11:53:50 +0100

    Fix __init__

--- partycls/descriptor/__init__.py
+++ partycls/descriptor/__init__.py
@@ -7,4 +7,5 @@ from .ba import BondAngleDescriptor
 from .smoothed_ba import SmoothedBondAngleDescriptor
 from .gr import RadialDescriptor
 from .bo import BondOrientationalDescriptor, LechnerDellagoDescriptor
+from .smoothed_bo import SmoothedBondOrientationalDescriptor
 from .dscribe import *
