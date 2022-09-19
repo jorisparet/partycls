6338ce104deab0646714e90cc0936bdfbed45068
Joris Paret <joris.paret@umontpellier.fr>
2022-09-19 13:50:03 +0200

    Add docstring

--- partycls/descriptor/tetra.py
+++ partycls/descriptor/tetra.py
@@ -3,8 +3,61 @@ from .descriptor import StructuralDescriptor, AngularStructuralDescriptor
 from .realspace_wrap import compute
 
 class TetrahedralDescriptor(AngularStructuralDescriptor):
-
-    name = 'bond-angle'
+    """
+    Tetrahedral descriptor.
+    
+    Computes the average deviation of the bond angles between a central 
+    particle i and all the pairs of its nearest neighbors (j,k) from the ideal 
+    angle in a tetrahedron (109.5°).
+    
+    This descriptor is scalar. Therefore, the `grid` attribute is not relevant. 
+    
+    See the parent class for more details.
+    
+    Parameters
+    ----------
+    
+    trajectory : str or an instance of `Trajectory`.
+        Trajectory on which the structural descriptor will be computed.
+        
+    
+    Attributes
+    ----------
+    
+    trajectory : Trajectory
+        Trajectory on which the structural descriptor will be computed.
+        
+    active_filters : list of str
+        All the active filters on both groups prior to the computation of the
+        descriptor.
+        
+    dimension : int
+        Spatial dimension of the descriptor (2 or 3).
+        
+    grid : array
+        Grid over which the structural features will be computed.
+        
+    features : ndarray
+        Array of all the structural features for the particles in group=0 in
+        accordance with the defined filters (if any). This attribute is 
+        initialized when the method `compute` is called (default value is None).
+        
+    cutoffs : list of float
+        List of cutoff distances to identify the nearest neighbors using
+        the fixed-cutoff ('FC') method.
+        
+    nearest_neighbors_method : str, default: 'FC'
+        Nearest neighbor method, 'FC' or 'SANN'.
+    
+    Examples:
+    ---------
+    
+    >>> D = TetrahedralDescriptor('trajectory.xyz')
+    >>> D.nearest_neighbors_method = 'FC'
+    >>> D.compute()
+    """
+    
+    name = 'tetrahedral'
     symbol = 'tetra'
     
     def __init__(self, trajectory):
