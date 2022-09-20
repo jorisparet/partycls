5bdb12008fa1e6cb45ffd9e0e7477ca939676ac5
Joris Paret <joris.paret@umontpellier.fr>
2022-09-20 20:31:14 +0200

    Minor changes and improve docstring

--- partycls/descriptor/smoothed_ba.py
+++ partycls/descriptor/smoothed_ba.py
@@ -33,17 +33,57 @@ class SmoothedBondAngleDescriptor(BondAngleDescriptor):
         Consider neighbors j `cutoff_enlargement * rc_ij` away from the central
         particle i.
         
-    power_law : int
-        Value `n` for the power law decay w(r).
+    exponent : int
+        Exponent `n` in the power law for the exponential decay in w(r).
+        
+    Attributes
+    ----------
+    
+    trajectory : Trajectory
+        Trajectory on which the structural descriptor will be computed.
+        
+    active_filters : list of str
+        All the active filters on both groups prior to the computation of the
+        descriptor.
+        
+    dimension : int
+        Spatial dimension of the descriptor (2 or 3).
+        
+    grid : array
+        Grid over which the structural features will be computed.
+        
+    features : ndarray
+        Array of all the structural features for the particles in group=0 in
+        accordance with the defined filters (if any). This attribute is 
+        initialized when the method `compute` is called (default value is None).
+        
+    cutoffs : list of float
+        List of enlarged cutoff distances to identify the nearest neighbors 
+        using the fixed-cutoff ('FC') method.
+        
+    standard_cutoffs_FC : list of float
+        List of standard cutoffs (i.e. not enlarged) with the fixed-cutoff 
+        ('FC') method.
+        
+    nearest_neighbors_method : str, default: 'FC'
+        Nearest neighbor method, 'FC' or 'SANN'.
+        
+    Examples:
+    ---------
+    
+    >>> D = SmoothedBondAngleDescriptor('trajectory.xyz', cutoff_enlargement=1.3, exponent=8)
+    >>> D.nearest_neighbors_method = 'SANN'
+    >>> D.add_filter("species == 'A'")
+    >>> D.compute()   
     """
 
     name = 'smoothed-bond-angle'
     symbol = 'sba'
 
-    def __init__(self, trajectory, dtheta=3.0, cutoff_enlargement=1.3, power_law=8):
+    def __init__(self, trajectory, dtheta=3.0, cutoff_enlargement=1.3, exponent=8):
         BondAngleDescriptor.__init__(self, trajectory, dtheta=3.0)
         self.cutoff_enlargement = cutoff_enlargement
-        self.power_law = power_law        
+        self.exponent = exponent        
 
     def compute(self):
         StructuralDescriptor._sanity_checks(self)
@@ -57,12 +97,21 @@ class SmoothedBondAngleDescriptor(BondAngleDescriptor):
         pairs = numpy.asarray(self.trajectory[0].pairs_of_species_id)
         features = numpy.empty((self.size, self.n_features), dtype=numpy.float64)
         row = 0
-        # override the computation of cutoffs from nearest_neighbors()
-        self._compute_cutoffs()
-        cutoffs = numpy.array(self.cutoffs)
+        
+        # compute cutoffs for the descriptor if not provided
+        if None in self.cutoffs:
+            if self.nearest_neighbors_method != 'FC':
+                print("Warning: using fixed cutoffs for the computation of \
+                      the descriptor. Neighbors are determined using the \
+                     `{}` method.".format(self.nearest_neighbors_method))
+            self._compute_cutoffs()
+        # store the standard FC cutoffs
+        self.standard_cutoffs_FC = numpy.array(self.cutoffs)
+        
         # compute nearest neighbors with enlarged cutoffs
         self.cutoffs = list(self.cutoff_enlargement * numpy.array(self.cutoffs))
         self.nearest_neighbors(method=self.nearest_neighbors_method)
+       
         for n in range(n_frames):
             box = self.trajectory[n].cell.side
             for i in range(len(idx_0[n])):
@@ -72,8 +121,8 @@ class SmoothedBondAngleDescriptor(BondAngleDescriptor):
                                                               pos_0[n][i], pos_1[n].T,
                                                               spe_0[n][i], spe_1[n],
                                                               neigh_i,
-                                                              pairs, cutoffs,
-                                                              self.power_law, box,
+                                                              pairs, self.standard_cutoffs_FC,
+                                                              self.exponent, box,
                                                               self.n_features,
                                                               self.dtheta)
                 features[row] = hist_n_i
