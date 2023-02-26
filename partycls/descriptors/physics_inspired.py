8b6bc52fb6532c2914a1bf080b1d3d2239d81f21
Joris Paret <joris.paret@gmail.com>
2023-02-26 11:18:51 +0100

    Add fixed version of physics-inspired descriptor with cutoff

--- partycls/descriptors/physics_inspired.py
+++ partycls/descriptors/physics_inspired.py
@@ -12,7 +12,7 @@ class PhysicsInspiredDescriptor(StructuralDescriptor):
     ...
 
     .. math::
-        X^\mathrm{C}(i) = (\: \:) .
+        X^\mathrm{P}(i) = (\: \:) .
     
     See the tutorials for more details.
 
@@ -49,23 +49,24 @@ class PhysicsInspiredDescriptor(StructuralDescriptor):
     name = 'physics-inspired'
     symbol = 'phins'
 
-    def __init__(self, trajectory, bounds=(1, 1.5), dr=0.1,
+    def __init__(self, trajectory, bounds=(1.0, 2.0), dr=0.5,
                  distance_grid=None,
+                 cutoff=None,
                  energy_field='potential_energy',
-                 cell_field='cell_perimeter',
+                 cell_field='cell_surface',
                  accept_nans=True, verbose=False):
         """
         Parameters
         ----------
-        bounds : tuple, default: (1, 1.5)
+        bounds : tuple, default: (1, 2)
             Lower and upper bounds :math:`(d_{n_\mathrm{min}}, d_{n_\mathrm{max}})`
-            to define the grid of distances :math:`\{ d_n \}`, where consecutive 
+            to define the grid of distances :math:`\{ L_n \}`, where consecutive 
             points in the the grid are separated by :math:`\Delta r`.
             
-        dr : float, default: 0.1
+        dr : float, default: 0.5
             Grid spacing :math:`\Delta r` to define the grid of distances 
-            :math:`\{ d_n \}` in the range 
-            :math:`(d_{n_\mathrm{min}}, d_{n_\mathrm{max}})` by steps of size 
+            :math:`\{ L_n \}` in the range 
+            :math:`(L_{n_\mathrm{min}}, d_{L_\mathrm{max}})` by steps of size 
             :math:`\Delta r`.
             
         distance_grid : list, default: None
@@ -73,10 +74,18 @@ class PhysicsInspiredDescriptor(StructuralDescriptor):
             from ``None``, it  overrides the linearly-spaced grid defined by 
             ``bounds`` and ``dr``.
 
+        cutoff : float, default: None
+            Distance up to which the particles contribute to the coarse-grained
+            average of the central particle. Particles beyond this cutoff distance
+            will be ignored, for efficiency reasons. This distance should be larger
+            than the largest distance in the grid :math:`\{ L_n \}` (large enough not to
+            discard significant contributions). If ``None``, the value will automatically
+            be chosen to be half the side of the simulation cell.
+
         energy_field : str, default: "potential_energy"
             Name of the particle property used to store the potential energy.
 
-        cell_field : str, default: "cell_perimeter"
+        cell_field : str, default: "cell_surface"
             Name of the particle property used to store the appropriate Voronoi
             cell metric (*i.e.* the cell perimeter in 2D and the cell surface in
             3D).
@@ -95,9 +104,15 @@ class PhysicsInspiredDescriptor(StructuralDescriptor):
                                       accept_nans=accept_nans,
                                       verbose=verbose)
         # dummy values, to be set in the following lines
-        #self._distance_grid = [-1]
         # set the grid of distances {r}
         self._set_bounds(dr, bounds, distance_grid)
+        # handle cutoff
+        sides = self.trajectory.dump('cell.side')
+        sides = numpy.asarray(sides)
+        if cutoff is None:
+            self.cutoff = numpy.min(sides) / 2
+        else:
+            self.cutoff = cutoff
         # fields
         self.energy_field = energy_field
         self.cell_field = cell_field
@@ -150,11 +165,18 @@ class PhysicsInspiredDescriptor(StructuralDescriptor):
         n_frames = len(self.trajectory)
         # all relevant arrays
         pos_0 = self.dump('position', group=0)
-        pos_1 = self.dump('position', group=1)
-        spe_1 = self.dump('species_id', group=1)
-        Epot_1 = self.dump('{}'.format(self.energy_field), group=1)
-        cell_1 = self.dump('{}'.format(self.cell_field), group=1)
+        pos_all = self.trajectory.dump('position')
+        spe_0 = self.dump('species_id', group=0)
+        spe_all = self.trajectory.dump('species_id')
+        Epot_0 = self.dump('particle.{}'.format(self.energy_field), group=0)
+        Epot_all = self.trajectory.dump('particle.{}'.format(self.energy_field))
+        cell_0 = self.dump('particle.{}'.format(self.cell_field), group=0)
+        cell_all = self.trajectory.dump('particle.{}'.format(self.cell_field))
         box = self.trajectory.dump('cell.side')
+        # compute extended neighbors with an extended cutoff
+        n_pairs = len(self.trajectory[0].pairs_of_species)
+        extended_cutoffs = numpy.array([self.cutoff for _ in range(n_pairs)])
+        self._compute_extended_neighbors(extended_cutoffs)
         # the descriptor is computed for each species ID separately (1, 2, ...)
         # and also for all species at once, which is expressed as -1.
         distinct_species = list(self.trajectory[0].distinct_species_id)
@@ -164,104 +186,31 @@ class PhysicsInspiredDescriptor(StructuralDescriptor):
         n_L = len(self._distance_grid)
         for n in self._trange(n_frames):
             pos_0_n = pos_0[n].T
-            pos_1_n = pos_1[n].T
-            spe_1_n = spe_1[n]
-            Epot_1_n = Epot_1[n]
-            cell_1_n = cell_1[n]
+            pos_all_n = pos_all[n].T
+            spe_0_n = spe_0[n],
+            spe_all_n = spe_all[n]
+            Epot_0_n = Epot_0[n]
+            Epot_all_n = Epot_all[n]
+            cell_0_n = cell_0[n]
+            cell_all_n = cell_all[n]
             npart = len(self.groups[0][n])
+            max_num_neigh_n = numpy.max(self._extended_neighbors_number[n])
             for spe_n, beta in enumerate(distinct_species):
                 feat_spe_n = compute.physics_inspired_all(self._distance_grid,
-                                                           pos_0_n, pos_1_n,
-                                                           spe_1_n, beta,
-                                                           Epot_1_n, cell_1_n,
-                                                           box[n])
+                                                          self._extended_neighbors[n],
+                                                          self._extended_neighbors_number[n],
+                                                          max_num_neigh_n,
+                                                          pos_0_n, pos_all_n,
+                                                          spe_0_n, spe_all_n, beta,
+                                                          Epot_0_n, Epot_all_n, 
+                                                          cell_0_n, cell_all_n,
+                                                          box[n])
                 self.features[start: start+npart, 4*n_L*spe_n: 4*n_L*(spe_n+1) ] = feat_spe_n
             start += npart
         self._handle_nans()
         return self.features
 
-
-    # def compute(self):
-    #     """
-    #     Compute the physics-informed descriptor for the particles in group=0.
-    #     Returns the data matrix and also overwrites the ``features`` attribute.
-
-    #     Returns
-    #     -------
-    #     features : numpy.ndarray
-    #         Data matrix.
-    #     """
-    #     # set up
-    #     self._set_up(dtype=numpy.float64)
-    #     n_frames = len(self.trajectory)
-    #     # all relevant arrays
-    #     pos_0 = self.dump('position', group=0)
-    #     pos_all = self.trajectory.dump('position')
-    #     spe_0 = self.dump('species_id', group=0)
-    #     spe_all = self.trajectory.dump('species_id')
-    #     Epot_0 = self.dump('particle.{}'.format(self.potential_energy_field), group=0)
-    #     Epot_all = self.trajectory.dump('particle.{}'.format(self.potential_energy_field))
-    #     cell_perimeter_0 = self.dump('particle.{}'.format(self.cell_perimeter_field), group=0)
-    #     cell_perimeter_all = self.trajectory.dump('particle.{}'.format(self.cell_perimeter_field))
-    #     box = self.trajectory.dump('cell.side')
-    #     # compute extended neighbors with extended cutoffs
-    #     # based on the largest distance in the distance grid
-    #     R_cut = self.distance_grid[-1] + self.skin
-    #     n_pairs = len(self.trajectory[0].pairs_of_species)
-    #     extended_cutoffs = numpy.array([R_cut for _ in range(n_pairs)])
-    #     self._compute_extended_neighbors(extended_cutoffs)
-    #     # the descriptor is computed for each species separately and
-    #     # also for all species at once.
-    #     # we use two separate lists to keep track of this:
-    #     # (1) a list of species_id [1, 2,, 3, ..., -1] (-1 = all).
-    #     # (2) a list of boolean [False, False, ..., True] to know
-    #     # if all species must be considered or not.
-    #     distinct_species = list(self.trajectory[0].distinct_species_id)
-    #     use_all_species = [False for _ in range(len(distinct_species))]
-    #     distinct_species += [-1]
-    #     use_all_species += [True]
-    #     # computation
-    #     start = 0
-    #     for n in self._trange(n_frames):
-    #         pos_0_n = pos_0[n].T
-    #         pos_all_n = pos_all[n].T
-    #         spe_0_n = spe_0[n]
-    #         spe_all_n = spe_all[n]
-    #         Epot_0_n = Epot_0[n]
-    #         Epot_all_n = Epot_all[n]
-    #         cell_perimeter_0_n = cell_perimeter_0[n]
-    #         cell_perimeter_all_n = cell_perimeter_all[n]
-    #         npart = len(self.groups[0][n])
-    #         count_L_spe = 0
-    #         M = numpy.max(self._extended_neighbors_number[n])
-    #         for L in self._distance_grid:
-    #             for spe_target, use_all in zip(distinct_species, use_all_species):
-    #                 feat_L_spe_n = compute.physics_inspired_all(L,
-    #                                                         self._extended_neighbors[n],
-    #                                                         self._extended_neighbors_number[n],
-    #                                                         pos_0_n,
-    #                                                         pos_all_n,
-    #                                                         spe_0_n,
-    #                                                         spe_all_n,
-    #                                                         spe_target,
-    #                                                         use_all,
-    #                                                         Epot_0_n,
-    #                                                         Epot_all_n,
-    #                                                         cell_perimeter_0_n,
-    #                                                         cell_perimeter_all_n,
-    #                                                         M,
-    #                                                         box[n])
-    #                 self.features[start: start+npart, 4*count_L_spe: 4*(count_L_spe+1)] = feat_L_spe_n
-    #                 count_L_spe += 1
-    #         start += npart
-    #     self._handle_nans()
-    #     return self.features
-
     def _set_bounds(self, dr, bounds, distance_grid):
-        # take the smallest side as maximal upper bound for the distance grid
-        sides = numpy.array(self.trajectory.get_property('cell.side'))
-        L = numpy.min(sides)
-        
         if distance_grid is not None:
             self._distance_grid = numpy.array(distance_grid, dtype=numpy.float64)
             self._dr = None
@@ -269,7 +218,7 @@ class PhysicsInspiredDescriptor(StructuralDescriptor):
         
         else:
             self._dr = dr
-            if len(bounds) == 2 and bounds[0] >= 0 and bounds[1] >= 0 and bounds[0] < bounds[1] and bounds[1] <= L / 2:
+            if len(bounds) == 2 and bounds[0] >= 0 and bounds[1] >= 0 and bounds[0] < bounds[1]:
                     rmin, rmax = bounds
                     r = numpy.arange(rmin, rmax + (self._dr / 2), self._dr, dtype=numpy.float64)
                     # set grid and bounds
